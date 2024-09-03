9aa4283bc483b32234ae1755fb6ec2acf9566cce
piaa033 <mptomeo@gmail.com>
2024-09-03 20:20:19 -0300

    ordenado

--- java.js
+++ java.js
@@ -9,18 +9,33 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .then(data => {
             console.log(data);
+            const items = data.items.sort((a, b) => a.name.localeCompare(b.name));
 
-            info = document.getElementById("información")
-            var adiciones = ""
-            for(i=0; i<data.items.length; i++){
-                let id = data.items[i].id
-                adiciones += `<img id="img_${id}" class="imagen" src="${data.items[i].image}"></img>`+`<p id="p_${id}">` + `<span>`+ "Nombre: " +`</span>` + data.items[i].name + `<br>`+
-                `<span>`+ "KI: " +`</span>` + data.items[i].ki + `<br>`+
-                `<span>`+ "KI Máximo: " +`</span>` + data.items[i].maxKi + `<br>`+
-                `<span>`+ "Raza: " +`</span>` + data.items[i].race + `<br>`+
-                `<span>`+ "Género: " +`</span>` + data.items[i].gender + `<br>`+
-                `<span>`+ "Descripción: " +`</span>` + data.items[i].description + `<br>`+
-                `<span>`+ "Afiliación: " +`</span>` + data.items[i].affiliation + `<br>`+`</p>`
-            };
+            // Obtén el elemento donde añadirás la información
+            const info = document.getElementById("información");
+
+            // Usa un string para acumular el HTML a insertar
+            let adiciones = "";
+
+            // Itera sobre los elementos en 'items'
+            for (let i = 0; i < items.length; i++) {
+                const item = items[i];
+                const id = item.id;
+                
+                adiciones += `
+                    <div>
+                        <img id="img_${id}" class="imagen" src="${item.image}" alt="${item.name}">
+                        <p id="p_${id}">
+                            <span>Nombre: </span>${item.name}<br>
+                            <span>KI: </span>${item.ki}<br>
+                            <span>KI Máximo: </span>${item.maxKi}<br>
+                            <span>Raza: </span>${item.race}<br>
+                            <span>Género: </span>${item.gender}<br>
+                            <span>Descripción: </span>${item.description}<br>
+                            <span>Afiliación: </span>${item.affiliation}<br>
+                        </p>
+                    </div>
+                `;
+            }
             info.innerHTML = adiciones})})
 
